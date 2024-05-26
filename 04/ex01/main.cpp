ea5d87db96e3233dc2cc8df2900ce763436d5f5a
rscres <rseelaen@student.42sp.org.br>
2024-05-26 11:34:37 -0300

    04 remove Wrong*.cpp from Makefile and related includes

--- 04/ex01/main.cpp
+++ 04/ex01/main.cpp
@@ -3,16 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: renato <renato@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 12:28:41 by renato            #+#    #+#             */
-/*   Updated: 2024/05/25 20:27:47 by renato           ###   ########.fr       */
+/*   Updated: 2024/05/26 11:33:37 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 #include "Cat.hpp"
-#include "WrongCat.hpp"
 #include <iostream>
 
 int main()
--- 04/ex02/main.cpp
+++ 04/ex02/main.cpp
@@ -3,16 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: renato <renato@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 12:28:41 by renato            #+#    #+#             */
-/*   Updated: 2024/05/25 21:36:39 by renato           ###   ########.fr       */
+/*   Updated: 2024/05/26 11:33:43 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 #include "Cat.hpp"
-#include "WrongCat.hpp"
 #include <iostream>
 #include <sstream>
 
