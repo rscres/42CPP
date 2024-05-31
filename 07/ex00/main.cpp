ae2063376de7485714412c0a0211a493406a8605
rscres <rseelaen@student.42sp.org.br>
2024-05-30 22:08:07 -0300

    07 add more tests

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -1,6 +1,30 @@
 #include "whatever.hpp"
 #include <iostream>
 
+class Awesome
+{
+  public:
+    Awesome(void) : _n(0) {}
+    Awesome( int n ) : _n( n ) {}
+	Awesome(const Awesome& other) : _n(other._n) {} // copy constructor
+    Awesome& operator=(const Awesome& other) { // copy assignment operator
+        if (this != &other) {
+            _n = other._n;
+        }
+        return *this;
+    }
+    bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+    bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+    bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+    bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+    bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+    bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+    int get_n() const { return _n; }
+  private:
+    int _n;
+};
+std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
 int main( void ) {
     int a = 2;
     int b = 3;
@@ -18,5 +42,11 @@ int main( void ) {
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
+	Awesome e(2), f(4);
+
+	::swap(e, f);
+	std::cout << e << " " << f << std::endl;
+	std::cout << ::max(e, f) << std::endl;
+	std::cout << ::min(e, f) << std::endl;
     return 0;
 }
--- 07/ex01/ex01.cpp
+++ 07/ex01/ex01.cpp
@@ -1,18 +1,3 @@
-#include "iter.hpp"
-#include <iostream>
-
-// void printInt(int i) {
-//     std::cout << i << std::endl;
-// }
-
-// int main() {
-//     int arr[5] = {1, 2, 3, 4, 5};
-//     ::iter(arr, 5, &printInt);
-    
-//     std::string arr2[3] = {"hello", "there", "mate"};
-//     ::iter(arr2, 3, ::printArr<std::string>);
-// }
-
 class Awesome
 {
   public:
