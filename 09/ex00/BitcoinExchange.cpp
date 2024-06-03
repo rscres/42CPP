c4d95b5f9400b45c740fce8f296afb3a2554344e
rscres <renatocrescenti@gmail.com>
2024-06-03 00:27:57 -0300

    09/ex00 add base files

---
+++ 09/ex00/BitcoinExchange.cpp
@@ -0,0 +1,23 @@
+
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() {
+    // Initialization code here
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) {
+    // Copy data from 'other' to this object
+}
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other) {
+    if (this != &other) {
+        // Free existing resources (if any)
+
+        // Copy data from 'other' to this object
+    }
+    return *this;
+}
+
+BitcoinExchange::~BitcoinExchange() {
+    // Cleanup code here
+}
---
+++ 09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,13 @@
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+class BitcoinExchange {
+    public:
+        BitcoinExchange();
+        BitcoinExchange(const BitcoinExchange& other);
+        BitcoinExchange& operator=(const BitcoinExchange& other);
+        ~BitcoinExchange();
+};
+
+#endif // BITCOINEXCHANGE_HPP
