c06037d5a93d3126da3c690c2d2876668cd32525
rscres <renatocrescenti@gmail.com>
2024-06-02 22:34:10 -0300

    08/ex00 add -std to Makefile, remove forward_list and array tests from main

--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -3,35 +3,46 @@
 #include <vector>
 #include <deque>
 #include <list>
-#include <forward_list>
-#include <array>
 
 int main(void) {
 
     std::cout << "--------VECTOR--------" << std::endl;
-    std::vector<int> v1{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<int> v1;
+    v1.push_back(0);
+    v1.push_back(1);
+    v1.push_back(2);
+    v1.push_back(3);
+    v1.push_back(4);
+    v1.push_back(5);
+    v1.push_back(6);
+    v1.push_back(7);
+    v1.push_back(8);
+    v1.push_back(9);
     ::easyfind(v1, 5);
     ::easyfind(v1, 11);
 
     std::cout << "---------DEQUE--------" << std::endl;
-    std::deque<int> d1{10, 13, 17, 21, 30, 42};
+    std::deque<int> d1;
+    d1.push_back(10);
+    d1.push_back(13);
+    d1.push_back(17);
+    d1.push_back(21);
+    d1.push_back(30);
+    d1.push_back(42);
     ::easyfind(d1, 11);
     ::easyfind(d1, 17);
 
-    std::cout << "---------ARRAY--------" << std::endl;
-    std::array<int, 7> a1{-1, 0, 1, 2, 3, 4, 5};
-    ::easyfind(a1, 5);
-    ::easyfind(a1, 11);
-
     std::cout << "----------LIST---------" << std::endl;
-    std::list<int> l1{-1, 0, 1, 2, 3, 4, 5};
+    std::list<int> l1;
+    l1.push_back(-1);
+    l1.push_back(0);
+    l1.push_back(1);
+    l1.push_back(2);
+    l1.push_back(3);
+    l1.push_back(4);
+    l1.push_back(5);
     ::easyfind(l1, 1);
     ::easyfind(l1, 11);
-
-    std::cout << "------FORWARD-LIST------" << std::endl;
-    std::forward_list<int> fl1{-1, 0, 1, 2, 3, 4, 5};
-    ::easyfind(fl1, 3);
-    ::easyfind(fl1, 11);
-    
+  
     return 0;
 }
