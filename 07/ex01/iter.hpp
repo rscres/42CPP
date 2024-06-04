89c85ad0b5e7b8de3ba273ca516ee4b3170e16e4
rscres <rseelaen@student.42sp.org.br>
2024-06-04 13:14:17 -0300

    07/ex01 modify tests

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -2,17 +2,18 @@
 #define ITER_HPP
 
 #include <cstddef>
-#include <iostream>
 
 template<typename T>
-void printArr(T t) {
-    std::cout << t << std::endl;
+void iter(T const *array, size_t size,T func(T const element)) {
+    for (size_t i = 0; i < size; i++) {
+        array[i] = func(array[i]);
+    }
 }
 
-template<typename T, typename F>
-void iter(T& array, size_t size, F func) {
+template<typename T>
+void iter(T *array, size_t size,T func(T element)) {
     for (size_t i = 0; i < size; i++) {
-        func(array[i]);
+        array[i] = func(array[i]);
     }
 }
 
