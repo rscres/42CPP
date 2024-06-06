40b84a8de67c6b3659a324d930027c6b447af0c5
rscres <renatocrescenti@gmail.com>
2024-06-05 21:43:41 -0300

    08/ex00 fetch upstream

--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -8,16 +8,8 @@ int main(void) {
 
     std::cout << "--------VECTOR--------" << std::endl;
     std::vector<int> v1;
-    v1.push_back(0);
-    v1.push_back(1);
-    v1.push_back(2);
-    v1.push_back(3);
-    v1.push_back(4);
-    v1.push_back(5);
-    v1.push_back(6);
-    v1.push_back(7);
-    v1.push_back(8);
-    v1.push_back(9);
+    for (int i = 0; i < 10; i++)
+		v1.push_back(i);
     ::easyfind(v1, 5);
     ::easyfind(v1, 11);
 
